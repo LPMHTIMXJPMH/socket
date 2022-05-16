1f43c42da93e8b0be78b95169741a71c91f31a63
HighDimension Cost <46214170+LPMHTIMXJPMH@users.noreply.github.com>
2022-05-16 23:38:57 +0800

    Update server.c

--- server-client-example-c-sockets-linux/server.c
+++ server-client-example-c-sockets-linux/server.c
@@ -21,7 +21,7 @@ int main(int argc , char *argv[])
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
+	server.sin_port = htons( 5049 );
 	
 	//Bind
 	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
